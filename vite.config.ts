ae21d4576cb17e71743b02dedd1606a14d043fe0
Abdullah Albanyan <albanyanab@gmail.com>
2025-12-31 17:33:04 +0300

    fix favicon again

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 export default defineConfig({
   plugins: [viteSingleFile()],
   root: "src",
+  base: "/riftbound-counter/",
   build: {
     outDir: "../public",
     emptyOutDir: false,
